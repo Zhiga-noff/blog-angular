f5e66434b2f38af83c20accc8313194ba7bc9943
kirill <zhiganov_k.n@mail.ru>
2024-05-05 23:04:47 +0300

    Реализовал страницу создания поста

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -24,6 +24,8 @@ export class LoginPageComponent implements OnInit {
     this.route.queryParams.subscribe((params) => {
       if (params['loginAgain']) {
         this.message = 'Пожалуйста, введите данные';
+      } else if (params['authFailed']) {
+        this.message = 'Сессия истекла. Введите данные заново';
       }
     });
     this.form = new FormGroup<any>({
